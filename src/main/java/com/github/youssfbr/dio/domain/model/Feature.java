b9578737ee698320b5531886c846a62399f20965
Alisson Youssf <youssfbr@gmail.com>
2023-10-15 20:03:50 -0300

    @EqualsAndHashCode(callSuper=false)

--- src/main/java/com/github/youssfbr/dio/domain/model/Feature.java
+++ src/main/java/com/github/youssfbr/dio/domain/model/Feature.java
@@ -2,9 +2,11 @@ package com.github.youssfbr.dio.domain.model;
 
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity(name = "tb_feature")
+@EqualsAndHashCode(callSuper=false)
 public class Feature extends BaseItem {
 
 }
--- src/main/java/com/github/youssfbr/dio/domain/model/News.java
+++ src/main/java/com/github/youssfbr/dio/domain/model/News.java
@@ -2,9 +2,11 @@ package com.github.youssfbr.dio.domain.model;
 
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity(name = "tb_news")
+@EqualsAndHashCode(callSuper=false)
 public class News extends BaseItem {
 
 }
